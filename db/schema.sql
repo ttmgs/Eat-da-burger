2a09550ea61179011a07e72c72676b59f80580cd
Magdi Tiea <magditiea@gmail.com>
2021-02-26 20:50:57 -0500

    all modules added

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (id INT PRIMARY KEY, burger_name VARCHAR(20) NOT NULL, devoured BOOLEAN)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured)
+('krusty krab', true),
+('cheesburger', false),
+('daves_single', true);
