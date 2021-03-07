ee418072f2ef6252fc9a8ec6841e8622a29abdba
Magdi Tiea <magditiea@gmail.com>
2021-03-07 00:05:23 -0500

    server.js file finalized

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var router = express.Router();
+
+// MAKE CALLS USING MODEL FUNCTIONS
+var model = require("../models/model.js");
+
+router.get("/", function(req, res){
+    var waitingList;
+    var devouredList;
+    model.all("waiting", function(data){
+        waitingList = data;
+        console.log(waitingList);
+        model.all("devoured", function(data){
+            devouredList = data;
+            console.log(devouredList);
+            res.render("index", {waitingList, devouredList});
+        });
+    });
+});
+
+router.post("/api/add-burgers", function(req, res){
+    var burgerName = req.body.name;
+    model.create(burgerName, function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+router.post("/api/devour-burgers/:id", function(req, res){
+    console.log("hit2");
+    var id  = req.params.id;
+    model.swapTables("waiting", "devoured", id, function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+module.exports = router;
